4bc77f33d8c096396f30c473067c32e5d7e66532
Kevin Scheeren <kevinscheeren@outlook.com>
2024-09-29 22:19:46 +0200

    added exports for types and annotations

--- lib/universal_js_interop.dart
+++ lib/universal_js_interop.dart
@@ -1,3 +1,5 @@
 library universal_js_interop;
 
-export 'src/universal_js_interop.dart' if (dart.library.html) 'dart:js_interop';
+export 'src/js_interop.dart' if (dart.library.html) 'dart:js_interop';
+export 'src/js_annotations.dart';
+export 'src/js_types.dart';

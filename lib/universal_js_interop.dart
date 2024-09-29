a85c4196cd86f7a9cfcecc9bf36a71ca4368c798
Kevin Scheeren <kevinscheeren@outlook.com>
2024-09-29 23:05:03 +0200

    completed conditional imports

--- lib/universal_js_interop.dart
+++ lib/universal_js_interop.dart
@@ -1,5 +1,7 @@
 library universal_js_interop;
 
+export 'src/typed_data.dart' if (dart.library.html) 'dart:typed_data';
+export 'src/js_util.dart' if (dart.library.html) 'dart:js_util';
 export 'src/js_interop.dart' if (dart.library.html) 'dart:js_interop';
 export 'src/js_annotations.dart';
 export 'src/js_types.dart';
